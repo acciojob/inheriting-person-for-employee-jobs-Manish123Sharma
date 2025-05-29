c716aa35de7eff9418c629c1a8f7c3140c8a1bad
Manish Kumar Sharma <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-29 11:47:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-  // Call the Person constructor to initialize name and age
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Set up inheritance from Person
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
 };
 
 // Do not change code below this line
